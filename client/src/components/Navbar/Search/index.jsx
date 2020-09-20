5a718582bb52b3f22265c5393fcf222ffcfee554
CGMcBride <connermcbride@ymail.com>
2020-09-20 13:25:15 -0500

    search displays books in search

--- client/src/components/Navbar/Search/index.jsx
+++ client/src/components/Navbar/Search/index.jsx
@@ -2,16 +2,38 @@ import React, { Component } from "react";
 
 import API from "../../../utils/API";
 import { Jumbotron, Form, FormControl, Button } from "react-bootstrap";
+// import { query } from "express";
 
 export default class index extends Component {
 	state = {
 		search: "",
 		books: [],
 	};
+	handleInput = (event) => {
+		// const name = event.target.name;
+		const value = event.target.value;
+		console.log(value);
+		// this.setState({
+		// 	[name]: value,
+		// });
+		this.setState({
+			search: value,
+		});
+	};
+	searchBook = (query) => {
+		API.getBooks(query)
+			.then((res) =>
+				this.setState({
+					books: res.data.items,
+				})
+			)
+			.catch((err) => console.error(err));
+	};
 	food = (event) => {
 		event.preventDefault();
-		const value = document.getElementById("search").value;
-		console.log(value);
+		// const value = document.getElementById("search").value;
+		// console.log(value);
+		this.searchBook(this.state.search);
 	};
 	render() {
 		return (
@@ -20,17 +42,45 @@ export default class index extends Component {
 					<h1 className="header">React Google Book Search</h1>
 					<h5>Search and save your books of intrest</h5>
 				</Jumbotron>
-				<Form inline className="justify-content-center">
+				{/* <Form inline className="justify-content-center">
 					<FormControl
 						type="text"
-						placeholder="Search"
+						placeholder="Search Your Book"
 						className="mr-sm-2 w-50"
-						search={this.state.search}
+						// search={this.state.search}
+						handleInputChange={this.handleInput}
+						// handleFormSubmit={this.food}
 					/>
 					<Button onClick={this.food} variant="outline-dark">
 						Search Your Book
 					</Button>
-				</Form>
+				</Form> */}
+				<form>
+					<input
+						type="text"
+						onChange={this.handleInput}
+						placeholder="Search a Book"
+					/>
+					<button onClick={this.food}>Search</button>
+				</form>
+
+				<div>
+					{this.state.books.map(
+						(book) => (
+							<div className="p-3 my-5 border border-dark" key={book.id}>
+								<h3>{book.volumeInfo.title}</h3>
+								<h6>{book.volumeInfo.subtitle}</h6>
+								<div className="row">
+									<div className="col-md-3">
+										<img src={book.volumeInfo.imageLinks.thumbnail} />
+									</div>
+									<div className="col-md-8">{book.volumeInfo.description}</div>
+								</div>
+							</div>
+						)
+						// console.log(book)
+					)}
+				</div>
 			</div>
 		);
 	}
